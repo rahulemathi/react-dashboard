a458415333d42332804ff945da8e9eb7c14d09fc
Rahul E <erahul096@gmail.com>
2022-06-01 19:00:06 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react"; //importing react from react
-import { ReactDOM } from "react";  //importing react from raect dom
+import  ReactDOM  from "react-dom";  //importing react from raect dom
 import App from "./App"; //import app from app
-ReactDOM.render(<App/>, document.getElementById('root'));
+import "./index.css" ;
+ReactDOM.render(<App />, document.getElementById("root"));
